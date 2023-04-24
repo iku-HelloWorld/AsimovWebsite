932194812a868c3e73f3e2b7031cfc2863f0a031
Enise007 <119949746+Enise007@users.noreply.github.com>
2023-04-24 12:43:30 +0300

    home screen html and css

--- anasayfa.js
+++ anasayfa.js
@@ -1,1 +1,8 @@
 "use strict";
+const sidebar = document.querySelector(".sidebar");
+const mainContent = document.querySelector(".main-content");
+
+document.querySelector(".mainscreeniconButton").onclick = function () {
+  sidebar.classList.toggle("sidebar_small");
+  mainContent.classList.toggle("main-content_large");
+};
