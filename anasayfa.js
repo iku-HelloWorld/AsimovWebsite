d422fd06214af03f485cd80d171407b371b5c4b7
eceozcan <119422829+eceozcan@users.noreply.github.com>
2023-04-25 20:46:56 +0300

    Update anasayfa.js

--- anasayfa.js
+++ anasayfa.js
@@ -1,1 +1,7 @@
 "use strict";
+
+const dotsDiv = document.querySelector(".dots");
+const dots = document.querySelectorAll(".dot");
+const slides = document.querySelectorAll(".slide");
+const arrowRight = document.querySelector(".arrow-right");
+const arrowLeft = document.querySelector(".arrow-left");
