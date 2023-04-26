091855083d0a4d7841347aeeb406aea4bd3d0094
Enise007 <119949746+Enise007@users.noreply.github.com>
2023-04-26 15:39:07 +0300

    Adding the sidebar menu

--- anasayfa.js
+++ anasayfa.js
@@ -1,8 +1,11 @@
 "use strict";
 const sidebar = document.querySelector(".sidebar");
-const mainContent = document.querySelector(".main-content");
-
-document.querySelector(".mainscreeniconButton").onclick = function () {
-  sidebar.classList.toggle("sidebar_small");
-  mainContent.classList.toggle("main-content_large");
+const mainContent = document.querySelector(".mainscreenSecond");
+const sidebarOpen = function () {
+  sidebar.classList.toggle("sidebar_show");
 };
+document
+  .querySelector(".mainscreeniconButton")
+  .addEventListener("click", sidebarOpen);
+
+document.querySelector(".go_back_btn").addEventListener("click", sidebarOpen);
