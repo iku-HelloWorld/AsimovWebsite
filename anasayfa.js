4e50f8e294ed566fe0ab0f8e5d05c5f58d8fb018
Ece <2200007216@stu.iku.edu.tr>
2023-04-29 12:06:38 +0300

    js added

--- anasayfa.js
+++ anasayfa.js
@@ -1,7 +1,35 @@
 "use strict";
 
-const dotsDiv = document.querySelector(".dots");
-const dots = document.querySelectorAll(".dot");
-const slides = document.querySelectorAll(".slide");
-const arrowRight = document.querySelector(".arrow-right");
-const arrowLeft = document.querySelector(".arrow-left");
+const page = document.querySelectorAll(".galeri_page");
+const prevBtn = document.querySelector(".arrow1");
+const nextBtn = document.querySelector(".arrow2");
+
+let index = 1;
+
+const displaySlider = function (i) {
+  if (index < 1) {
+    index = page.length;
+  }
+  if (index > page.length) {
+    index = 1;
+  }
+
+  console.log(index);
+  page.forEach((p) => p.classList.remove("show"));
+  page.item(index - 1).classList.add("show");
+};
+
+const changeSlide = function (n) {
+  index += n;
+  displaySlider(index);
+};
+
+prevBtn.addEventListener("click", function () {
+  changeSlide(-1);
+});
+
+nextBtn.addEventListener("click", function () {
+  changeSlide(1);
+});
+
+displaySlider(index);
