a9fabab4509449ed92ca19174da5192e6bcad219
Ece <2200007216@stu.iku.edu.tr>
2023-05-12 10:57:38 +0300

    slider entegration

--- hakkımızda/hakkımızda.js
+++ hakkımızda/hakkımızda.js
@@ -1,1 +1,28 @@
 "use strict";
+const slider = document.querySelector('.slider');
+const slides = slider.querySelector('.slides');
+const slideWidth = slider.clientWidth;
+
+let slideIndex = 0;
+
+function slideNext() {
+  slideIndex++;
+  if (slideIndex >= slides.children.length) {
+    slideIndex = 0;
+  }
+  updateSlidePosition();
+}
+
+function slidePrevious() {
+  slideIndex--;
+  if (slideIndex < 0) {
+    slideIndex = slides.children.length - 1;
+  }
+  updateSlidePosition();
+}
+
+function updateSlidePosition() {
+  slides.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
+}
+
+setInterval(slideNext, 3000);
