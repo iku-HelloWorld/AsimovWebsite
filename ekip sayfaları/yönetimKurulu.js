38b784188b3249f9c95f7f454b5b99aba555dc8d
sudxq <115904461+sudxq@users.noreply.github.com>
2023-05-17 11:44:50 +0300

    navbar js code
    navbar js code added

--- ekip sayfaları/yönetimKurulu.js
+++ ekip sayfaları/yönetimKurulu.js
@@ -1,1 +1,11 @@
 "use strict";
+/*navbar*/
+const button = document.getElementById(`list-btn`);
+const menu = document.querySelector(`.asimov-menu`);
+
+if (button) {
+  button.addEventListener(`click`, () => {
+    menu.classList.toggle(`active`);
+  });
+}
+/*navbar*/
