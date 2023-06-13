b1bff7eae5153cc6ed8b15c1375d7930a8c90de9
RaseL2546 <ahmetcelalkoc_60_09@hotmail.com>
2023-06-13 20:12:59 +0300

    firebase first entegrate

---
+++ hakkımızda/hakkımızdaFb.js
@@ -0,0 +1,17 @@
+"use strict";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+
+const database = getDatabase();
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCIQyW2X33YimVFujydd3ycfrmuC-oLYzo",
+  authDomain: "asimov-website.firebaseapp.com",
+  projectId: "asimov-website",
+  storageBucket: "asimov-website.appspot.com",
+  messagingSenderId: "693182885740",
+  appId: "1:693182885740:web:a8450b8b14a41579778d49",
+  measurementId: "G-HS93FSYNKF",
+};
+
+const app = initializeApp(firebaseConfig);
