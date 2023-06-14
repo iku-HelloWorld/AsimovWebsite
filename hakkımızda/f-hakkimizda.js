24684559007c191423e4193cea81bc76a1c685b8
BaranKayaalp <barancalosa@gmail.com>
2023-06-14 21:45:20 +0300

    Firebase
    Firebase

---
+++ hakkımızda/f-hakkimizda.js
@@ -0,0 +1,30 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-storage.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCIQyW2X33YimVFujydd3ycfrmuC-oLYzo",
+  authDomain: "asimov-website.firebaseapp.com",
+  projectId: "asimov-website",
+  storageBucket: "asimov-website.appspot.com",
+  messagingSenderId: "693182885740",
+  appId: "1:693182885740:web:a8450b8b14a41579778d49",
+  measurementId: "G-HS93FSYNKF",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase()
+const analytics = getAnalytics(app);
+const auth = getAuth();
+const storage = getStorage(app);
