43f84bd78c43128d9b537870ac5e7636f6fde925
Ece <2200007216@stu.iku.edu.tr>
2023-06-25 11:53:30 +0300

    etkinliklerfb update

--- admin/etkinliklerFb.js
+++ admin/etkinliklerFb.js
@@ -6,34 +6,91 @@ import {
   set,
   get,
   child,
+  remove,
 } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
 
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
-const etkinliklerRef = ref(database, "etkinlikler"); // Reference to the 'etkinlikler' node in the database
-
-const yeniEtkinlik = {
-  başlık: "New Event Title",
-  açıklama: "New Event Description",
+const firebaseConfig = {
+  apiKey: "AIzaSyCIQyW2X33YimVFujydd3ycfrmuC-oLYzo",
+  authDomain: "asimov-website.firebaseapp.com",
+  databaseURL:
+    "https://asimov-website-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "asimov-website",
+  storageBucket: "asimov-website.appspot.com",
+  messagingSenderId: "693182885740",
+  appId: "1:693182885740:web:a8450b8b14a41579778d49",
+  measurementId: "G-HS93FSYNKF",
 };
 
-submit.addEventListener("click", function () {
-  signInWithEmailAndPassword(auth, username.value, password.value)
-    .then((userCredential) => {
-      if (userCredential) {
-        push(etkinliklerRef, yeniEtkinlik)
-          .then(() => {
-            console.log("New event added successfully!");
-          })
-          .catch((error) => {
-            console.error("Error adding new event:", error);
-          });
-      }
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    });
-});
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+const db = getDatabase();
+const storage = getStorage();
 
+// sil
+
+const etkinlilerRemove = document.querySelector(
+  ".etkinlikler-remove"
+);
+
+get(child(dbRef, `etkinlikler/`))
+  .then((snapshot) => {
+    if (snapshot.exists()) {
+      // console.log(Object.entries(snapshot.val()));
+
+      Object.entries(snapshot.val()).forEach((e) => {
+        const ad = e[1].isim;
+        const nick = e[1].etkinlikler;
+
+        etkinliklerRemove.insertAdjacentHTML(
+          "afterbegin",
+          `<div>
+              <button class="etkinlikler-remove">etkinliği sil</button>
+            </div>`
+        );
+      });
+    } else {
+      // console.log("No data available");
+    }
+
+    // console.log(etkinlikler);
+  })
+  .catch((error) => {
+    // console.error("Etkinlikler yüklenemedi");
+  });
+
+  etkinliklerImg = document.querySelector(
+    ".etkinlikler-img"
+  );
+  const etkinliklerSubmit = document.querySelector(
+    ".etkinlikler-submit"
+  );
+
+  const uploadTask = uploadBytes(imageRef, resim, metadata).then(
+    (snapshot) => {
+      // getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+      //   console.log("File available at", downloadURL);
+      // });
+      // console.log("success");
+      location.reload();
+      corporateCommunicationMemberName.value = "";
+      corporateCommunicationMemberImg.value = "";
+    }
+  );
+
+const etkinliklerRef = ref(database, "etkinlikler"); // Reference to the 'etkinlikler' node in the database
+
+const dbRef = ref(getDatabase());
+get(child(dbRef, `etkinlikler/`)).then((snapshot) => {
+  if (snapshot.exists()) {
+    console.log(snapshot.val());
+  } else {
+    console.log("No data available");
+  }
+}).catch((error) => {
+  console.error(error);
+});
 // etkinliklerimiz
