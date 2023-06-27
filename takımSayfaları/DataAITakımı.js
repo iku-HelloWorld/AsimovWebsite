4d4cd9b63197162b259a0c8a4ecb8082832750c4
Enise007 <119949746+Enise007@users.noreply.github.com>
2023-06-27 13:28:16 +0300

    takım sayfaları json

--- takımSayfaları/DataAITakımı.js
+++ takımSayfaları/DataAITakımı.js
@@ -1,4 +1,5 @@
 "use strict";
+import json from "/language.json" assert { type: "json" };
 
 const button = document.getElementById(`list-btn`);
 const menu = document.querySelector(`.asimov-menu`);
@@ -25,3 +26,59 @@ window.addEventListener("scroll", function () {
     // console.log("bye");
   }
 });
+
+const handleLang = function () {
+  if (lang === "Tr") {
+    lang = json.Tr.lang;
+    // navbar
+    for (let key in json.Tr.navbar) {
+      if (document.getElementById(`${key}`)) {
+        document.getElementById(
+          `${key}`
+        ).textContent = `${json.Tr.navbar[key]}`;
+      } else {
+        console.log(key + " key in json is not accesable in this page");
+      }
+    }
+    for (let key in json.Tr.dataaiTakımı) {
+      if (document.getElementById(`${key}`)) {
+        document.getElementById(
+          `${key}`
+        ).textContent = `${json.Tr.dataaiTakımı[key]}`;
+      } else {
+        console.log(key + "is not accesable");
+      }
+      // console.log(document.getElementById(`${key}`), json.En.navbar[key]);
+    }
+  } else {
+    lang = json.En.lang;
+    // navbar
+    for (let key in json.En.navbar) {
+      if (document.getElementById(`${key}`)) {
+        document.getElementById(
+          `${key}`
+        ).textContent = `${json.En.navbar[key]}`;
+      } else {
+        console.log(key + "is not accesable");
+      }
+      // console.log(document.getElementById(`${key}`));
+    }
+
+    // mainscreen
+    for (let key in json.En.dataaiTakımı) {
+      if (document.getElementById(`${key}`)) {
+        document.getElementById(
+          `${key}`
+        ).textContent = `${json.En.dataaiTakımı[key]}`;
+      } else {
+        console.log(key + " not exist");
+      }
+      // console.log(document.getElementById(`${key}`));
+    }
+  }
+};
+
+textBtn.addEventListener("click", function () {
+  handleAnimation();
+});
+handleLang();
